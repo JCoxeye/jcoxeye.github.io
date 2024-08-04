61188a38a1a45f6a745284ddf662d249b1e4df62
JCoxeye <oxeye1284@gmail.com>
2024-08-04 19:56:32 -0400

    added lore pit roulette

---
+++ scripting/lorePitRoulette.js
@@ -0,0 +1,461 @@
+function lorePitRoulette()
+{
+	for (var elem of document.getElementsByClassName("rouletteX"))
+	{
+		switch (Math.ceil(Math.random() *50))
+		{
+			case 1:
+				tempContent = `Kris Dreemurr`;
+			break;
+			
+			case 2:
+				tempContent = `Susie`;
+			break;
+			
+			case 3:
+				tempContent = `Ralsei`;
+			break;
+			
+			case 4:
+				tempContent = `Noelle Holiday`;
+			break;
+			
+			case 5:
+				tempContent = `Asgore Dreemurr`;
+			break;
+			
+			case 6:
+				tempContent = `Toriel Dreemurr`;
+			break;
+			
+			case 7:
+				tempContent = `Asriel Dreemurr`;
+			break;
+			
+			case 8:
+				tempContent = `Rudy Holiday`;
+			break;
+			
+			case 9:
+				tempContent = `Mayor Holiday`;
+			break;
+			
+			case 10:
+				tempContent = `Dess Holiday`;
+			break;
+			
+			case 11:
+				tempContent = `Sans`;
+			break;
+			
+			case 12:
+				tempContent = `Papyrus`;
+			break;
+			
+			case 13:
+				tempContent = `W.D. Gaster`;
+			break;
+			
+			case 14:
+				tempContent = `Gerson Boom`;
+			break;
+			
+			case 15:
+				tempContent = `Alvin Boom`;
+			break;
+			
+			case 16:
+				tempContent = `Ms. Boom`;
+			break;
+			
+			case 17:
+				tempContent = `Lancer`;
+			break;
+			
+			case 18:
+				tempContent = `Rouxls Kaard`;
+			break;
+			
+			case 19:
+				tempContent = `Seam`;
+			break;
+			
+			case 20:
+				tempContent = `Jevil`;
+			break;
+			
+			case 21:
+				tempContent = `King`;
+			break;
+			
+			case 22:
+				tempContent = `Swatch`;
+			break;
+			
+			case 23:
+				tempContent = `Tasque Manager`;
+			break;
+			
+			case 24:
+				tempContent = `Queen`;
+			break;
+			
+			case 25:
+				tempContent = `Tenna`;
+			break;
+			
+			case 26:
+				tempContent = `Mike`;
+			break;
+			
+			case 27:
+				tempContent = `Berdly`;
+			break;
+			
+			case 28:
+				tempContent = `Catti`;
+			break;
+			
+			case 29:
+				tempContent = `Jockington`;
+			break;
+			
+			case 30:
+				tempContent = `Bratty`;
+			break;
+			
+			case 31:
+				tempContent = `Catty`;
+			break;
+			
+			case 32:
+				tempContent = `Alphys`;
+			break;
+			
+			case 33:
+				tempContent = `QC`;
+			break;
+			
+			case 34:
+				tempContent = `Burgerpants`;
+			break;
+			
+			case 35:
+				tempContent = `Pizzapants`;
+			break;
+			
+			case 36:
+				tempContent = `Bugerpant`;
+			break;
+			
+			case 37:
+				tempContent = `ICE-E`;
+			break;
+			
+			case 38:
+				tempContent = `Everyman`;
+			break;
+			
+			case 39:
+				tempContent = `Friend`;
+			break;
+			
+			case 40:
+				tempContent = `The Eggman`;
+			break;
+			
+			case 41:
+				tempContent = `The Player`;
+			break;
+			
+			case 42:
+				tempContent = `The Knight`;
+			break;
+			
+			case 43:
+				tempContent = `The Angel`;
+			break;
+			
+			case 44:
+				tempContent = `The Vessel`;
+			break;
+			
+			case 45:
+				tempContent = `Frisk`;
+			break;
+			
+			case 46:
+				tempContent = `Chara`;
+			break;
+			
+			case 47:
+				tempContent = `Tera`;
+			break;
+			
+			case 48:
+				tempContent = `Acter`;
+			break;
+			
+			case 49:
+				tempContent = `The Third Entity`;
+			break;
+			
+			case 50:
+				tempContent = `Oberon Smog`;
+			break;
+		};
+		elem.innerHTML = tempContent;
+	};
+	for (var elem of document.getElementsByClassName("rouletteY"))
+	{
+		switch (Math.ceil(Math.random() * 23))
+		{
+			case 1:
+				tempContent = `Gaster connections`;
+			break;
+			
+			case 2:
+				tempContent = `biblical connections`;
+			break;
+			
+			case 3:
+				tempContent = `connections to the main cast`;
+			break;
+			
+			case 4:
+				tempContent = `no connection to the main cast`;
+			break;
+			
+			case 5:
+				tempContent = `clear implications for the narrative`;
+			break;
+			
+			case 6:
+				tempContent = `thematic relevance`;
+			break;
+			
+			case 7:
+				tempContent = `"thematic relevance"`;
+			break;
+			
+			case 8:
+				tempContent = `'Gaster's Theme' as the first melodic content of their theme`;
+			break;
+			
+			case 9:
+				tempContent = `a secret <i>Penumbra Phantasm</i> quote in their theme`;
+			break;
+			
+			case 10:
+				tempContent = `long hands`;
+			break;
+			
+			case 11:
+				tempContent = `a closet-sized body`;
+			break;
+			
+			case 12:
+				tempContent = `boots`;
+			break;
+			
+			case 13:
+				tempContent = `killed Asriel`;
+			break;
+			
+			case 14:
+				tempContent = `killed Dess`;
+			break;
+			
+			case 15:
+				tempContent = `trauma`;
+			break;
+			
+			case 16:
+				tempContent = `died`;
+			break;
+			
+			case 17:
+				tempContent = `extra-textual knowledge`;
+			break;
+			
+			case 18:
+				tempContent = `meta-textual knowledge`;
+			break;
+			
+			case 19:
+				tempContent = `knowledge of Gaster`;
+			break;
+			
+			case 20:
+				tempContent = `knowledge of the player`;
+			break;
+			
+			case 21:
+				tempContent = `no object permanence`;
+			break;
+			
+			case 22:
+				tempContent = `existed since 2012`;
+			break;
+			
+			case 23:
+				tempContent = `moved between timelines/realities`;
+			break;
+		};
+		elem.innerHTML = tempContent;
+	};
+	for (var elem of document.getElementsByClassName("rouletteZ"))
+	{
+		switch (Math.ceil(Math.random() * 36))
+		{
+			case 1:
+				tempContent = `the Knight`;
+			break;
+			
+			case 2:
+				tempContent = `OBVIOUSLY the Knight`;
+			break;
+			
+			case 3:
+				tempContent = `the Angel`;
+			break;
+			
+			case 4:
+				tempContent = `THE PENUMBRA NEO ANGEL`;
+			break;
+			
+			case 5:
+				tempContent = `Gaster`;
+			break;
+			
+			case 6:
+				tempContent = `the Letter Writer`;
+			break;
+			
+			case 7:
+				tempContent = `the Eggman`;
+			break;
+			
+			case 8:
+				tempContent = `REDACTED`;
+			break;
+			
+			case 9:
+				tempContent = `Mysteryman`;
+			break;
+			
+			case 10:
+				tempContent = `the Third Entity`;
+			break;
+			
+			case 11:
+				tempContent = `UNUSED`;
+			break;
+			
+			case 12:
+				tempContent = `IMAGE_FRIEND`;
+			break;
+			
+			case 13:
+				tempContent = `Mike`;
+			break;
+			
+			case 14:
+				tempContent = `Tenna`;
+			break;
+			
+			case 15:
+				tempContent = `secretly a Dreemurr`;
+			break;
+			
+			case 16:
+				tempContent = `secretly a Holiday`;
+			break;
+			
+			case 17:
+				tempContent = `PILES of DUST`;
+			break;
+			
+			case 18:
+				tempContent = `a dustner`;
+			break;
+			
+			case 19:
+				tempContent = `a goner`;
+			break;
+			
+			case 20:
+				tempContent = `a secret boss`;
+			break;
+			
+			case 21:
+				tempContent = `the secret boss of <i>Chapter 3</i>`;
+			break;
+			
+			case 22:
+				tempContent = `the final secret boss`;
+			break;
+			
+			case 23:
+				tempContent = `the main antagonist`;
+			break;
+			
+			case 24:
+				tempContent = `associated with orange`;
+			break;
+			
+			case 25:
+				tempContent = `associated with Gaster`;
+			break;
+			
+			case 26:
+				tempContent = `a representation of Kris' inner turmoil`;
+			break;
+			
+			case 27:
+				tempContent = `a Toby Fox self-insert`;
+			break;
+			
+			case 28:
+				tempContent = `a Gaster reference`;
+			break;
+			
+			case 29:
+				tempContent = `the demon of Babylon, disguised with the coat of the righteous`;
+			break;
+			
+			case 30:
+				tempContent = `a <i>Homestuck</i> reference`;
+			break;
+			
+			case 31:
+				tempContent = `behind the slaughter`;
+			break;
+			
+			case 32:
+				tempContent = `doomed by the narrative`;
+			break;
+			
+			case 33:
+				tempContent = `evidence for Papyrus Knight`;
+			break;
+			
+			case 34:
+				tempContent = `evidence for Third Entity`;
+			break;
+			
+			case 35:
+				tempContent = `evidence for Friend Letter`;
+			break;
+			
+			case 36:
+				tempContent = `evidence for Kris Knight`;
+			break;
+			
+			case 37:
+				tempContent = `the Player`;
+			break;
+		};
+		elem.innerHTML = tempContent;
+	};
+};
